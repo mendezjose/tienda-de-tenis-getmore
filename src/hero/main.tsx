d8beea80ff923aee531294b2fb464659929a9b73
Jose Mendez <iscjosemendez@gmail.com>
2024-12-22 15:16:32 -0600

    clean repo

--- src/hero/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import Hero from './Hero.tsx';
-
-createRoot(document.getElementById('hero')!).render(
-  <StrictMode>
-    <Hero />
-  </StrictMode>
-);
--- src/navbar/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import Navbar from './Navbar.tsx';
-
-createRoot(document.getElementById('navbar')!).render(
-  <StrictMode>
-    <Navbar />
-  </StrictMode>
-);
